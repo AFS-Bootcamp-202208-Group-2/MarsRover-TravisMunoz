fb4ea124d5eaaef9ce28e8789a6432b3654ffb72
munoztr <munoztr@oocl.com>
2022-09-01 17:08:41 +0800

    Test: Command M when Direction S

--- mars-rover-tdd-2022-8-31-14-25-24-618/src/test/java/com/afs/tdd/DemoTest.java
+++ mars-rover-tdd-2022-8-31-14-25-24-618/src/test/java/com/afs/tdd/DemoTest.java
@@ -19,4 +19,19 @@ class DemoTest {
         assertEquals(expected_direction, marsRover.getDirection());
 
     }
+
+    @Test
+    void Should_return_0neg1N_when_command_is_M_given_00S() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        marsRover.command("M");
+
+        Integer expected_x = 0;
+        Integer expected_y = -1;
+        String expected_direction = "N";
+
+        assertEquals(expected_x, marsRover.getX_axis());
+        assertEquals(expected_y, marsRover.getY_axis());
+        assertEquals(expected_direction, marsRover.getDirection());
+
+    }
 }
