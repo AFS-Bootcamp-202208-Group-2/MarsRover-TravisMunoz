b2cb694bf25f3d800873b398a95beada833ea5e4
munoztr <munoztr@oocl.com>
2022-09-01 17:01:17 +0800

    Test: Command M

--- mars-rover-tdd-2022-8-31-14-25-24-618/src/test/java/com/afs/tdd/DemoTest.java
+++ mars-rover-tdd-2022-8-31-14-25-24-618/src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,21 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void Should_return_01N_when_command_is_M_given_00N() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        marsRover.command("M");
+
+        Integer expected_x = 0;
+        Integer expected_y = 1;
+        String expected_direction = "N";
+
+        assertEquals(expected_x, marsRover.getX_axis());
+        assertEquals(expected_y, marsRover.getY_axis());
+        assertEquals(expected_direction, marsRover.getDirection());
 
     }
 }
---
+++ mars-rover-tdd-2022-8-31-14-25-24-618/src/test/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,42 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    private int x_axis;
+    private int y_axis;
+    private String direction;
+
+    public MarsRover(int x_axis, int y_axis, String direction) {
+        this.x_axis = x_axis;
+        this.y_axis = y_axis;
+        this.direction = direction;
+    }
+
+    public int getX_axis() {
+        return x_axis;
+    }
+
+    public void setX_axis(int x_axis) {
+        this.x_axis = x_axis;
+    }
+
+    public int getY_axis() {
+        return y_axis;
+    }
+
+    public void setY_axis(int y_axis) {
+        this.y_axis = y_axis;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void command(String command) {
+
+    }
+}
